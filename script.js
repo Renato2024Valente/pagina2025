7f68fc94feb1509952c77e52b10d7b757adf4093
Renato2024Valente <161965660+Renato2024Valente@users.noreply.github.com>
2025-03-31 16:16:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+photoGroups.push({
+    title: "Nova Categoria",
+    photos: [
+      {
+        src: "path/to/photo1.jpg",
+        title: "Título da Foto 1",
+        text: "Descrição detalhada da foto 1."
+      },
+      {
+        src: "path/to/photo2.jpg",
+        title: "Título da Foto 2",
+        text: "Descrição detalhada da foto 2."
+      }
+    ]
+  });
+  
